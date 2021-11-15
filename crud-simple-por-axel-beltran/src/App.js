cbe4b52797f91a80cf76548c2320210a753984f6
axelll493 <beltran.axel.493@gmail.com>
2021-11-15 15:57:11 -0300

    Form

--- crud-simple-por-axel-beltran/src/App.js
+++ crud-simple-por-axel-beltran/src/App.js
@@ -7,10 +7,20 @@ function App() {
       <hr/>
       <div className = "row">
         <div className = "col-8">
-          Lista de tareas
+          <h4 className = "text-center">Listas De Tareas</h4>
+          <ul className = "list-group">
+            <li className = "list-group-item">
+              <span className = "lead">Nombre De La Tarea</span>
+              <button className = "btn btn-danger btn-sm mx-2">Eliminar</button>
+              <button className = "btn btn-warning btn-sm">Editar</button>
+            </li>
+          </ul>
         </div>
         <div className = "col-4">
-          formulario
+        <h4 className= "text-center">Formulario</h4>
+        <form>
+          <input type = "text" className = "form-control mb-2"/>
+        </form>
         </div>
       </div>
     </div>
